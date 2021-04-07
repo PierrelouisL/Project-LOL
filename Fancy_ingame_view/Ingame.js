1b7e9150d37365c354924e71aa578abc68c9fdb7
Pierre-Louis <pierrelouis.lagunegrand@gmail.com>
2021-04-07 22:51:24 +0200

    ajout fct nb champ

--- Fancy_ingame_view/Ingame.js
+++ Fancy_ingame_view/Ingame.js
@@ -1,22 +1,34 @@
 function Look_player() {
     var Username = document.getElementsByClassName("username_in")[0].value;
     var region = document.getElementById("regions").value;
-    var summoner_icon;
-    var summoner_id;
-    var summoner_accId;
+    console.log(Which_Champ("84"));
     $.getJSON('https://ds257qd73l.execute-api.eu-west-3.amazonaws.com/rgapi/summoner/euw1/' + Username + '/1', function (data) { /*Problème de CORS à cause de la region*/
-        summoner_icon = `${data.profileIconId}`;
-        summoner_id = `${data.id}`;
-        summoner_accId = `${data.accountId}`;
-        //alert(`${data.summonerLevel}`);
         GetUserInfos(`${data.profileIconId}`, `${data.id}`, `${data.accountId}`, `${data.summonerLevel}`)
-
     });
 }
 
 function GetUserInfos(icon, id, accId, level) {
     var image = document.getElementById("summonerIcon");
-    var srcimage = "https://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/" + summoner_icon + ".png";
-    //alert(srcimage);
+    var srcimage = "https://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/" + icon + ".png";
     image.src = srcimage;
+}
+
+function Which_Champ(id_champ) {
+    var champion;
+    $.ajax({
+        url: "https://ddragon.leagueoflegends.com/cdn/11.6.1/data/fr_FR/champion.json",
+        async: false,
+        dataType: 'json',
+        success: function (d) {
+            let championList = d.data;
+            for (var i in championList) {
+                if (championList[i].key == id_champ) {
+                    champion = championList[i].id;
+                    return champion;
+                }
+                //console.log(championList[i].id + " | " + championList[i].key);
+            }
+        }
+    });
+    return champion;
 }

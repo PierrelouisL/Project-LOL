5ca995fe4082359f8311118def32f76018dfce8f
Pierre-Louis <pierrelouis.lagunegrand@gmail.com>
2021-04-10 16:11:55 +0200

    Commit test

--- Fancy_ingame_view/Ingame.js
+++ Fancy_ingame_view/Ingame.js
@@ -1,16 +1,51 @@
+document.getElementsByClassName("username_in")[0].addEventListener('keydown', function (event) {
+    //console.log(event);
+    if (event.key == "Enter") {
+        Look_player();
+    } else {
+        return false;
+    }
+});
+
+document.getElementsByClassName("search_button")[0].addEventListener('click', function () {
+    console.log('test');
+    Look_player();
+});
+
 function Look_player() {
     var Username = document.getElementsByClassName("username_in")[0].value;
     var region = document.getElementById("regions").value;
-    console.log(Which_Champ("84"));
+    //console.log(Which_Champ("84"));
     $.getJSON('https://ds257qd73l.execute-api.eu-west-3.amazonaws.com/rgapi/summoner/euw1/' + Username + '/1', function (data) { /*Problème de CORS à cause de la region*/
         GetUserInfos(`${data.profileIconId}`, `${data.id}`, `${data.accountId}`, `${data.summonerLevel}`)
     });
+
 }
 
 function GetUserInfos(icon, id, accId, level) {
     var image = document.getElementById("summonerIcon");
+    console.log(icon);
+    console.log(id);
+    console.log(accId);
+    console.log(level);
     var srcimage = "https://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/" + icon + ".png";
     image.src = srcimage;
+    SetGameData(id);
+}
+
+function SetGameData(id) {
+    document.getElementById("rightarray").style.visibility = 'visible';
+    document.getElementById("leftarray").style.visibility = 'visible';
+    document.getElementsByClassName("username_in")[0].style.visibility = 'hidden';
+    document.getElementsByClassName("search_button")[0].style.visibility = 'hidden';
+    document.getElementsByClassName("form-control")[0].style.visibility = 'hidden';
+    $.getJSON('https://ds257qd73l.execute-api.eu-west-3.amazonaws.com/rgapi/summoner/euw1/' + id + '/10', function (data) {
+        let playerlist = data.participants;
+        for (var i in playerlist) {
+            //document.getElementById("pseudo" + i).innerHTML = playerlist[i].summonerName.key;
+            console.log(playerlist[i].summonerName.key);
+        }
+    });
 }
 
 function Which_Champ(id_champ) {
